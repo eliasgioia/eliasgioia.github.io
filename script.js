e5bd46712f2e030bfc24493a60090e5f1ee4b0ff
Elias Gioia <eliasgioia7@gmail.com>
2025-05-07 22:31:09 -0300

    pulido de la seccion experience (descripcion de trabajos)

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const timelineItems = document.querySelectorAll('.timeline-item');
+    const timelineLine = document.getElementById('timeline-line');
+
+    const totalItems = timelineItems.length;
+
+    timelineItems.forEach((item, index) => {
+      setTimeout(() => {
+        const dot = item.querySelector('.timeline-dot');
+        dot.classList.add('visible');
+
+        const itemOffset = item.offsetTop + item.offsetHeight / 2;
+        timelineLine.style.height = `${itemOffset}px`;
+      }, index * 500); // animación secuencial
+    });
+  });
